636747e166af48262433f20d190a62659690cc3f
NickPerezCarletonUniversity <nickperez@cmail.carleton.ca>
2020-11-18 17:38:37 -0500

    enabled learning rate search

--- train_evaluate.py
+++ train_evaluate.py
@@ -11,37 +11,23 @@ from layers import *
 import click
 from datetime import datetime
 
-#args
-@click.command()
-##Data args
-@click.option("-d","--datasetname", default="mnist", type=click.Choice(['cifar10','mnist', 'fashion_mnist']))
-@click.option("--n_classes", default=10)
-##Training args
-@click.option('--model_name', default='lenetKNN')
-@click.option('--kernel', default='polynomial')
-@click.option('--trainable_kernel', default='false')
-@click.option('--pooling_method', default='max') 
-@click.option('--cp', default=1.0)
-@click.option('--dp', default=3.0)
-@click.option('--gamma', default=1.0)
-@click.option("--batch_size", default=50)
-@click.option("--epochs", default=20)
-@click.option("--lr", default=0.003)
-@click.option("--keep_prob", default=1.0)
-@click.option("--weight_decay", default=0.0) 
-##logging args
-@click.option("-o","--base_log_dir", default="logs")
+def get_time_str():
+    #https://www.programiz.com/python-programming/datetime/current-datetime
+    # datetime object containing current date and time
+    now = datetime.now()
+    return now.strftime("%d_%m_%Y_%H_%M_%S")
 
-def main(datasetname,n_classes,batch_size,
+def get_trainable_str(trainable_kernel):
+    trainable_str = ""
+    if trainable_kernel:
+        trainable_str = "_trainable"  
+        
+    return trainable_str
+
+def train_and_evaluate(datasetname,n_classes,batch_size,
          model_name, kernel, trainable_kernel, cp, dp, gamma,pooling_method,
          epochs,lr,keep_prob,weight_decay,
-         base_log_dir):
-
-    #Fix TF random seed
-    tf.random.set_seed(1777)
-    log_dir = os.path.join(os.path.expanduser(base_log_dir),
-                           "{}".format(datasetname))
-    os.makedirs(log_dir, exist_ok=True)
+         log_dir):
     
     print('Using the ' + datasetname + ' dataset.')
 
@@ -89,50 +75,31 @@ def main(datasetname,n_classes,batch_size,
     #Run
 
     ep_cnt = tf.Variable(initial_value=0, trainable=False, dtype=tf.int64)
+    
+    dt_string = get_time_str()
+    
+    trainable_str = get_trainable_str(trainable_kernel)
 
     #Summary writers
     train_summary_writer = tf.summary.create_file_writer(os.path.join(log_dir,
                                                                       'summaries',
                                                                       'train'))
-    test_summary_writer = tf.summary.create_file_writer(os.path.join(log_dir,
-                                                                     'summaries',
-                                                                     'test'))
-    
-    #https://www.programiz.com/python-programming/datetime/current-datetime
-    # datetime object containing current date and time
-    now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-    
-    trainable_str = ""
-    if trainable_kernel:
-        trainable_str = "_trainable"
-                                                                     
     granular_test_summary_writer = tf.summary.create_file_writer(os.path.join(log_dir,
-                                                                     'summaries',
-                                                                      dt_string,
-                                                                      kernel + trainable_str,
-                                                                     'granular_test'))
+                                                                 'summaries',
+                                                                 'test',
+                                                                  dt_string,
+                                                                  kernel + trainable_str,
+                                                                 'granular_test'))                                               
 
-    ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=optimizer, net=model)
-    ckpt_path = os.path.join(log_dir, 'checkpoints')
-    manager = tf.train.CheckpointManager(ckpt,ckpt_path, max_to_keep=3)
-    #don't restore for now...
-    #ckpt.restore(manager.latest_checkpoint)
-    if manager.latest_checkpoint:
-        print("Restored from {}".format(manager.latest_checkpoint))
-    else:
-        print("Initializing from scratch.")
-
-
+    best_test_accuracy = 0
+    best_test_accuracy_to_return = 0
+    
     for ep in tqdm.trange(epochs, desc='Epoch Loop'):
         if ep < ep_cnt:
             continue
 
             # update epoch counter
         ep_cnt.assign_add(1)
-        
-        #with granular_test_summary_writer.as_default():
-        #    tf.summary.scalar("accuracy", 0, step=0)
         
         with train_summary_writer.as_default():
             # train for an epoch
@@ -142,13 +109,8 @@ def main(datasetname,n_classes,batch_size,
                 tf.summary.image("input_image", x, step=optimizer.iterations)
                 loss, logits = train_step(x,y)
                 train_acc_metric(y, logits)
-                ckpt.step.assign_add(1)
                 tf.summary.scalar("loss", loss, step=optimizer.iterations)
                 
-                if int(ckpt.step) % 1000 == 0:
-                    save_path = manager.save()
-                    print("Saved checkpoint for step {}: {}".format(int(ckpt.step),
-                                                                    save_path))
                 # Log every 100 batch
                 if (step + 1) % 100 == 0:
                     train_acc = train_acc_metric.result() 
@@ -170,6 +132,10 @@ def main(datasetname,n_classes,batch_size,
                         tf.summary.scalar("accuracy", test_acc, step=(step + 1) + ep * nrof_steps_per_epoch)
                         test_acc_metric.reset_states()
                         print('[Step {}] Test acc: {}'.format(step + 1, float(test_acc)))
+                        if best_test_accuracy <= float(test_acc):
+                            best_test_accuracy = float(test_acc)
+                            best_test_accuracy_to_return = test_acc
+                        print("best test accuracy so far: " + str(best_test_accuracy))
 
 
             # Display metrics at the end of each epoch.
@@ -178,21 +144,72 @@ def main(datasetname,n_classes,batch_size,
             print('Training acc over epoch: %s' % (float(train_acc),))
             # Reset training metrics at the end of each epoch
             train_acc_metric.reset_states()
- 
+            
+    return best_test_accuracy
 
-    ############################## Test the model #############################
-        with test_summary_writer.as_default():
-            for x_batch, y_batch in test_dataset:
-                if len(x_batch.shape)==3:
-                    x_batch = tf.expand_dims(x_batch, 3)
-                test_logits = model(x_batch, training=False)
-                # Update test metrics
-                test_acc_metric(y_batch, test_logits)
+#args
+@click.command()
+##Data args
+@click.option("-d","--datasetname", default="mnist", type=click.Choice(['cifar10','mnist', 'fashion_mnist']))
+@click.option("--n_classes", default=10)
+##Training args
+@click.option('--model_name', default='lenetKNN')
+@click.option('--kernel', default='polynomial')
+@click.option('--trainable_kernel', default='False')
+@click.option('--pooling_method', default='max') 
+@click.option('--cp', default=1.0)
+@click.option('--dp', default=3.0)
+@click.option('--gamma', default=1.0)
+@click.option("--batch_size", default=50)
+@click.option("--epochs", default=20)
+@click.option("--lr", default=0.003)
+@click.option("--keep_prob", default=1.0)
+@click.option("--weight_decay", default=0.0)
+@click.option("--lr_search", default='False') 
+##logging args
+@click.option("-o","--base_log_dir", default="logs")          
+def main(datasetname,n_classes,batch_size,
+         model_name, kernel, trainable_kernel, cp, dp, gamma,pooling_method,
+         epochs,lr,keep_prob,weight_decay,lr_search,
+         base_log_dir):
+    
+    
+    log_dir = os.path.join(os.path.expanduser(base_log_dir),
+                           "{}".format(datasetname))
+    os.makedirs(log_dir, exist_ok=True)
+    
+    #Fix TF random seed
+    tf.random.set_seed(1777)
+    
+    trainable_str = get_trainable_str(trainable_kernel)
 
-            test_acc = test_acc_metric.result()
-            tf.summary.scalar("accuracy", test_acc, step=ep)
-            test_acc_metric.reset_states()
-            print('[Epoch {}] Test acc: {}'.format(ep, float(test_acc)))
+    if lr_search.lower()=='true':
+        lr_search_test_summary_writer = tf.summary.create_file_writer(os.path.join(log_dir,
+                                                                 'summaries',
+                                                                 'learning_rate_searches',
+                                                                  get_time_str(),
+                                                                  kernel + trainable_str,
+                                                                 'granular_test'))
+        current_lr = 0.1
+        epochs = 1
+        current_lr_step = 0
+        while current_lr >= 0.001:
+            print("current_lr: " + str(current_lr))
+            best_accuracy = train_and_evaluate(datasetname,n_classes,batch_size,
+                                               model_name, kernel, trainable_kernel, cp, dp, gamma,pooling_method,
+                                               epochs,current_lr,keep_prob,weight_decay,
+                                               log_dir)
+            print("best_accuracy: " + str(best_accuracy))
+            with lr_search_test_summary_writer.as_default():
+                tf.summary.scalar("accuracy", best_accuracy, step=current_lr_step)
+            current_lr = current_lr / 2
+            current_lr_step = current_lr_step + 1
+    else:
+        train_and_evaluate(datasetname,n_classes,batch_size,
+                           model_name, kernel, trainable_kernel, cp, dp, gamma,pooling_method,
+                           epochs,lr,keep_prob,weight_decay,
+                           log_dir)
+
 
 if __name__=="__main__":
     main()
